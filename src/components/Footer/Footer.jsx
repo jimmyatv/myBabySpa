6e7d2e533778be825483bdb60d00e6715920aed9
jimmyatv <rasic.alexandar@gmail.com>
2023-11-30 22:22:22 +0100

    Footer | 80%

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,58 @@
-import React from 'react';
-import './Footer.css';
-import babyLogoWhite from '../../assets/babyLogoWhite.svg'
-import Header from '../Header/Header';
+import React from "react";
+import "./Footer.css";
+import babyLogoWhite from "../../assets/babyLogoWhite.svg";
+import Header from "../Header/Header";
+import { FiPhone } from "react-icons/fi";
+import { HiOutlineAtSymbol } from "react-icons/hi2";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div className="footer">
+      <div className="footer-wraper container">
         <div className="logo-white">
-            <img src={babyLogoWhite} alt="" />
+          <img src={babyLogoWhite} alt="" />
         </div>
-        <Header/>
+        <Header />
+        <div className="footer-bottom">
+          <div>
+            <span>Radno vreme:</span>
+            <span>Pon - Pet od 11:00 do 19:00</span>
+            <span>Subota od 10:00 do 14:00</span>
+            <span>
+              <FiPhone />+ 381 69 233 69 23
+            </span>
+            <span>
+              <HiOutlineAtSymbol />
+              info@babyspa.rs
+            </span>
+          </div>
+          <div>
+            <span>
+              <FaFacebookF />
+            </span>
+            <span>
+              <FaInstagram />
+            </span>
+            <p>
+              <FaRegCopyright /> Sva prava su zadržana
+            </p>
+          </div>
+          <div>
+            <p>Sajt razvijen</p>
+            <p>Aleksandar Rasic</p>
+            <span>
+              <FaFacebookF />
+            </span>
+            <span>
+              <FaInstagram />
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Footer;
