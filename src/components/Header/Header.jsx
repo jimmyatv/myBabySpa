5011fb7644760e67a57314d861d671bd943406ca
jimmyatv <rasic.alexandar@gmail.com>
2023-12-20 11:01:34 +0100

    Burger menu | ADDED -  70%

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/babyLogo.svg";
 import navBarData from "../../data/navBarData";
 
 const Header = () => {
+  // Dodao useState za pracenje stanja aktivnosti burger menija
+  const [isBurgerActive, setIsBurgerActive] = useState(false);
+
+  // Dodao funkciju za rukovanje klikom na burger meni
+  const handleBurgerClick = () => {
+    setIsBurgerActive((prev) => !prev);
+  };
+
   return (
     <div className="header">
       <div className="navBar">
         <div className="logo">
           <img src={Logo} alt="" />
         </div>
-        <div className="navBar-links">
+        <div
+          className={`navBar-links ${isBurgerActive ? "is-active" : ""}`}
+          id="navBarLinks"
+        >
           <ul>
             {navBarData.map((link, idx) => {
               return (
@@ -22,10 +33,23 @@ const Header = () => {
           </ul>
         </div>
         <div className="working-hours">
-          <span> <strong>+ 381 60 411 29 83</strong> </span>
+          <span>
+            {" "}
+            <strong>+ 381 60 411 29 83</strong>{" "}
+          </span>
           <span>Pon - Pet od 11:00 do 19:00</span>
           <span>Subota od 10:00 do 14:00</span>
         </div>
+        {/* Dodao event handler za burger klik */}
+        <button
+          className={`burger ${isBurgerActive ? "is-active" : ""}`}
+          id="burgerMenu"
+          onClick={handleBurgerClick}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
       </div>
     </div>
   );
