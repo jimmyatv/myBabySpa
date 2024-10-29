528a1183f392aa589a7d7de0ff0f5413d0d3c392
jimmyatv <rasic.alexandar@gmail.com>
2024-10-29 11:14:58 +0100

    Cname | CHANGED

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -74,7 +74,7 @@ const Header = () => {
       className={`header ${navBar ? 'navBar-active' : ''}`}>
       <div className="navBar">
         <div className="logo">
-          Mare<img src={Logo} alt="" />
+          <img src={Logo} alt="" />
         </div>
         <div
           className={`navBar-links ${isBurgerActive ? "is-active" : ""}`}
