a4d3d1142449ef719d5f05ee4e91466e6c729971
jimmyatv <rasic.alexandar@gmail.com>
2025-02-03 00:40:28 +0100

    Video loader correction | CHANGED

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Hero.css";
 import Header from "../Header/Header";
 import { motion } from "framer-motion";
@@ -6,23 +6,41 @@ import heroVideo from "../../assets/heroVideo.mp4"; // Uvezi video
 
 const Hero = () => {
   const transition = { type: "spring", duration: 3 };
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false); 
+
+  const handleVideoLoad = () => {
+    setIsVideoLoaded(true); 
+  };
 
   return (
     <div className="hero">
       <Header />
       {/* Video Background */}
-      <video className="hero-video" autoPlay loop muted playsInline disablePictureInPicture
-        disableRemotePlayback>
+      <video 
+        className="hero-video" 
+        autoPlay 
+        loop 
+        muted 
+        playsInline 
+        disablePictureInPicture
+        disableRemotePlayback
+        onCanPlayThrough={handleVideoLoad}
+      >
         <source src={heroVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      
+      {!isVideoLoaded && (
+        <div className="loader"></div>
+      )}
+      
       {/* Main Hero */}
       <div className="hero-main">
         <div className="the-best-babyspa">
           <motion.div
             initial={{ left: "160px" }}
             whileInView={{ left: "8px" }}
-            transition={{ ...transition, type: "tween",delay:2 }}
+            transition={{ ...transition, type: "tween", delay: 2 }}
           ></motion.div>
           <span>
             Najbolji <span>baby spa</span> u gradu
